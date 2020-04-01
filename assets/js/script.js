596bf18b50308dcc6c36ee62698cdfceed262d7e
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-04-01 13:37:49 -0700

    edit functionality complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,19 @@ var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector(".page-content");
+var completeEditTask = function(taskName, taskType, taskId) {
+  // find the matching task list item
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  // set new values
+  taskSelected.querySelector("h3.task-name").textContent = taskName;
+  taskSelected.querySelector("span.task-type").textContent = taskType;
 
+  alert("Task Updated!");
+  console.log(taskName, taskType, taskId);
+
+  formEl.removeAttribute("data-task-id");
+  document.querySelector("#save-task").textContent = "Add Task";
+};
 
 
 
@@ -64,12 +76,29 @@ var taskFormHandler = function(event) {
 
   formEl.reset ();
   
-  var taskFormData = {
-    name: taskNameInput,
-    type: taskTypeInput
-  };
+  var isEdit = formEl.hasAttribute("data-task-id");
+  console.log(isEdit);
 
-  createTaskEl(taskFormData);
+  //has data attribute, so get task id and call function to complete edit process
+  if (isEdit) {
+    var taskId = formEl.getAttribute("data-task-id");
+    completeEditTask(taskNameInput, taskTypeInput, taskId);
+  }
+    else {
+      var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+      };
+
+      createTaskEl(taskDataObj);
+    }
+  
+  //var taskFormData = {
+    //name: taskNameInput,
+    //type: taskTypeInput
+  //};
+
+  //createTaskEl(taskFormData);
 };
 
 var createTaskEl = function(taskDataObj) {
