afdaef7473e0c16192ff6de89d814266102d5ac6
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-04-01 09:43:59 -0700

    delete functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector(".page-content");
+
+
+
+
+var taskButtonHandler = function(event) {
+  console.log(event.target);
+
+  if(event.target.matches(".delete-btn")) {
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+
+var deleteTask = function(taskId) {
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+};
+
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -82,9 +101,12 @@ var createTaskActions = function(taskId) {
   
   return actionContainerEl;
 
-
-
 };
+
+
+//addEventLister below
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 
 formEl.addEventListener("submit", taskFormHandler);
 
